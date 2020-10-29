4d4b4f44f461f6a1191f9a8ed6403db8daf9fd09
eduardaelvira <fbio.alves095@gmail.com>
2020-10-29 20:51:34 -0300

    ...

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/imagens/*": {"origins": "*"}})
+
+@app.route('/')
+def delete():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
